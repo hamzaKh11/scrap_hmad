458f200da94a1d1490ac904e321f7d68ebdc9489
hamza khalid <79707600+hamzaKh11@users.noreply.github.com>
2023-06-04 17:42:03 +0100

    Update hmad.py
    i comment all of code

--- hmad.py
+++ hmad.py
@@ -1,8 +1,8 @@
-from bs4 import BeautifulSoup
-import cfscrape
-import pyrebase
+#from bs4 import BeautifulSoup
+#import cfscrape
+#import pyrebase
 
-firebaseConfig = {
+#firebaseConfig = {
   "apiKey": "AIzaSyB-KG5bkwn8imusmD97WXhLJ3WMUohjKhI",
   "authDomain": "dblematin-fb174.firebaseapp.com",
   "databaseURL": "https://dblematin-fb174-default-rtdb.firebaseio.com",
@@ -12,42 +12,42 @@ firebaseConfig = {
   "appId": "1:85079753433:web:2cac6ff88a5cde247d34c8",
   "measurementId": "G-TG09MT9HZF"
 }
-firebase = pyrebase.initialize_app(firebaseConfig)
-db = firebase.database()
+#firebase = pyrebase.initialize_app(firebaseConfig)
+#db = firebase.database()
     
-URL = "https://lematin.ma/"
-scraper = cfscrape.create_scraper()  
-soup=scraper.get(URL).content  
-soup=BeautifulSoup(soup,'html.parser')
+#URL = "https://lematin.ma/"
+#scraper = cfscrape.create_scraper()  
+#soup=scraper.get(URL).content  
+#soup=BeautifulSoup(soup,'html.parser')
 
-for div in soup.find_all("div",attrs={"class":"card bg-dark text-white bg-bloc"}): 
-    for link in div.find_all('a'):  
-        url = link.get('href')
-        page = scraper.get(url).content  
-        post = BeautifulSoup(page,'html.parser')
-        title = post.find('h1', {'id':'title'}).getText()
-        times = post.find('time').text
-        img = post.find('img', {'class':'d-block w-100'})
-        if img:
-          image = img.get('src')
-        texts = ''
-        for div in post.find_all("div",attrs={"class":"card-body p-2"}):
-            for p in div.findAll('p'):
-                texts += p.getText()
+#for div in soup.find_all("div",attrs={"class":"card bg-dark text-white bg-bloc"}): 
+#    for link in div.find_all('a'):  
+#        url = link.get('href')
+#        page = scraper.get(url).content  
+#        post = BeautifulSoup(page,'html.parser')
+#        title = post.find('h1', {'id':'title'}).getText()
+#       times = post.find('time').text
+ #       img = post.find('img', {'class':'d-block w-100'})
+ #      if img:
+  #        image = img.get('src')
+  #      texts = ''
+  #      for div in post.find_all("div",attrs={"class":"card-body p-2"}):
+  #          for p in div.findAll('p'):
+  #              texts += p.getText()
                 
-        data = {
+  #      data = {
             "title": title,
             "time" : times,
             "text" : texts,
             "image" : image
         }
 
-        posts = db.get()
-        titles = []
-        for i in range(len(posts.val())):
-          titles.append(posts[i].val()['title'])
+   #     posts = db.get()
+   #     titles = []
+   #     for i in range(len(posts.val())):
+    #      titles.append(posts[i].val()['title'])
 
-        if title in titles:
-          print('post already exist')
-        else:
-          print('db.push(data)')
+     #   if title in titles:
+      #    print('post already exist')
+       # else:
+        #  print('db.push(data)')
